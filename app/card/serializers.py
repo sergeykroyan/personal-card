ce0faec46ed3d395632c41a963a7520877639b3e
sergeykroyan <sergeykroyan@gmail.com>
2022-10-28 18:38:21 +0400

    creating validation in serializers

--- app/card/serializers.py
+++ app/card/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import Card
 from rest_framework.validators import UniqueTogetherValidator
@@ -10,5 +11,19 @@ class PersonalCardSerializer(ModelSerializer):
                   'age', 'gender', 'vaccinated')
 
     def create(self, validated_data):
-        instance, _ = Card.objects.get_or_create(**validated_data)
-        return instance
+        try:
+            value = Card.objects.get(name=validated_data['name'], middle_name=validated_data['middle_name'],
+                                     last_name=validated_data['last_name'])
+        except Card.DoesNotExist:
+            return Card.objects.create(**validated_data)
+        else:
+            return value
+
+    @staticmethod
+    def validate_age(value: int):
+        """
+        Check that age is meaningful for a living person
+        """
+        if 12 <= value <= 90:
+            return value
+        raise serializers.ValidationError("Age must be in the following range (12, 90)")
